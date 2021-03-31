33f9d0e4a2ed91d68c34ce40a1d0880f8db3f191
unknown <rahzy24@gmail.com>
2021-03-31 15:21:41 +0100

    Learning JS

---
+++ main.js
@@ -0,0 +1,56 @@
+// let val = document.getElementById('main');
+// let lis = document.getElementById('task-title');
+// let list; 
+
+
+// document.querySelector('ul li:first-child').style.background = "gold";
+// document.querySelector('ul li:nth-child(2)').style.background = "red";
+// document.querySelector('ul li:nth-child(3)').style.background = "yellow";
+// document.querySelector('ul li:nth-child(4)').style.background = "purple";
+// document.querySelector('ul li:nth-child(5)').style.background = "green";
+// document.querySelector('ul li:nth-child(5)').innerHTML = "wghrusnf";
+
+ 
+// let val = document.getElementsByClassName('card-title');
+
+// let list = document.querySelector('.collection').getElementsByClassName('collection-item');
+// let tag = document.getElementsByTagName('li');
+
+// let taag = Array.from(tag)
+
+// taag.forEach(tag => {
+//     console.log(tag)
+//     tag.textContent = "red";
+// });
+
+// for(i = 0; i < tag.length; i++){
+//     console.log(taag);
+//     tag[i].style.background = "green";
+//     tag[i].textContent = "green";
+// }
+
+
+// let alle = document.querySelectorAll('li');
+
+
+// for(i = 0; i < alle.length; i++){
+//  alle[i].style.background = "red"
+// }
+
+//  alle.forEach(alle => {
+//    console.log(alle);
+//   alle.style.background = "yellowgreen";
+// });
+
+// let lisOdd = document.querySelectorAll('collection-items:nth-child(odd)');
+// let liEven = document.querySelectorAll('li:nth-child(even)');
+
+// console.log(lisOdd);
+
+// lisOdd.forEach(lisOdd =>{
+//     lisOdd.style.background = 'red'
+// });
+
+// liEven.forEach(liEven =>{
+//     liEven.style.background = 'green'
+// });
