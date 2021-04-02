5de2d289a81c12cf12eba4d875b44056e9e5938e
unknown <rahzy24@gmail.com>
2021-04-03 00:46:47 +0100

    Learning JS

---
+++ DOMprojects.js
@@ -0,0 +1,47 @@
+//Define UI Variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const fliter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+const popup = document.querySelector('.pop');
+
+
+//Load All eventListners
+loadEventListeners();
+
+//Load all event listners
+function loadEventListeners() {
+    //Add task event
+    form.addEventListener('submit', addTask);
+}
+
+//Add Task
+function addTask(e) {
+    if(taskInput.value === ""){
+      popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+
+    //create list item
+    const lis = document.createElement('li');
+    const link = document.createElement('a');
+
+
+    lis.className = "collection-items";
+    link.className = "delete-item secondary-content";
+    
+
+    lis.appendChild(document.createTextNode(taskInput.value));
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    taskList.appendChild(lis);
+
+    lis.appendChild(link);
+
+    taskInput.value = "";
+
+
+    e.preventDefault();
+}
