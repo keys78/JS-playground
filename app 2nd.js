b3bb63ed1773460d4ac3fdcbfd6ec6e9c8da13f0
unknown <rahzy24@gmail.com>
2021-04-02 18:46:38 +0100

    New Learn

--- app 2nd.js
+++ app 2nd.js
@@ -41,5 +41,47 @@
 // }
 
 
-//LOCAL STORAGE
-localStorage.setItem('name', 'John');
+// //LOCAL STORAGE
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', '30');
+
+// //set sesion storage item
+// sessionStorage.setItem('name', 'Kim');
+
+// //remove from local storage
+// localStorage.removeItem('name')
+
+
+
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
+
+// localStorage.clear();
+
+// console.log(name,age)
+document.querySelector('form').addEventListener('submit',
+ function(e){
+    const task = document.getElementById('task').value;
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+
+    alert('task saved')
+
+    e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(task =>{
+    console.log(task);
+})
