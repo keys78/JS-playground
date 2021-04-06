c474b7ded0615640073706c13b04e4796d410294
unknown <rahzy24@gmail.com>
2021-04-06 03:10:01 +0100

    Learning

--- app 2nd.js
+++ app 2nd.js
@@ -59,29 +59,34 @@
 // localStorage.clear();
 
 // console.log(name,age)
-document.querySelector('form').addEventListener('submit',
- function(e){
-    const task = document.getElementById('task').value;
+// document.querySelector('form').addEventListener('submit',
+//  function(e){
+//     const task = document.getElementById('task').value;
 
-    let tasks;
+//     let tasks;
 
-    if(localStorage.getItem('tasks') === null){
-        tasks = [];
-    } else{
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
+//     if(localStorage.getItem('tasks') === null){
+//         tasks = [];
+//     } else{
+//         tasks = JSON.parse(localStorage.getItem('tasks'));
+//     }
 
-    tasks.push(task);
+//     tasks.push(task);
 
-    localStorage.setItem('tasks',JSON.stringify(tasks));
+//     localStorage.setItem('tasks',JSON.stringify(tasks));
 
-    alert('task saved')
+//     alert('task saved')
 
-    e.preventDefault();
-});
+//     e.preventDefault();
+// });
 
-const tasks = JSON.parse(localStorage.getItem('tasks'));
+// const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-tasks.forEach(task =>{
-    console.log(task);
-})
+// tasks.forEach(task =>{
+//     console.log(task);
+// })
+
+
+// const x = document.getElementById('main');
+
+// x.innerHTML = "red";
